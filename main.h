ce70aa74e97b7908d414bb79fc1751785895aa83
Erick Awino <erick.otieno89@gmail.com>
2023-04-26 07:12:28 +0000

    The Main header file

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* The Standard C header files */
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+
+
+/* The Function Prototypes */
+int _printf(const char *format, ...);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
